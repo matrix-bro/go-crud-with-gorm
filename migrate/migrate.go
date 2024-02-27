fc2abe1edcd220c72fa636b45d3bd220054d7898
matrix-bro <razmhz11@gmail.com>
2024-02-27 20:48:36 +0545

    created course and student models

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -12,7 +12,13 @@ func init() {
 }
 
 func main() {
-	err := initializers.DB.AutoMigrate(&models.User{}, &models.Profile{}, &models.Author{}, &models.Book{})
+	err := initializers.DB.AutoMigrate(&models.User{},
+		&models.Profile{},
+		&models.Author{},
+		&models.Book{},
+		&models.Course{},
+		&models.Student{},
+	)
 
 	if err != nil {
 		panic("Failed to perform migrations: " + err.Error())
---
+++ models/course.go
@@ -0,0 +1,17 @@
+package models
+
+import "gorm.io/gorm"
+
+// Many to Many Relationship
+type Course struct {
+	gorm.Model
+	Name     string
+	Students []*Author `gorm:"many2many:course_student;"`
+}
+
+type Student struct {
+	gorm.Model
+	FirstName string
+	LastName  string
+	Courses   []*Course `gorm:"many2many:course_student;"`
+}
