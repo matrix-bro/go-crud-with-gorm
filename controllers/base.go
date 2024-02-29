6f1fa785536d9837b0699321841f642728b8d174
matrix-bro <razmhz11@gmail.com>
2024-02-29 17:04:38 +0545

    error handling

--- controllers/base.go
+++ controllers/base.go
@@ -3,7 +3,18 @@ package controllers
 import (
 	"example/go-crud/initializers"
 	"example/go-crud/utils"
+	"fmt"
+	"reflect"
 )
+
+// getModelName returns the name of the type of the provided value.
+func getModelName(model interface{}) string {
+	t := reflect.TypeOf(model)
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+	return t.Name()
+}
 
 func CheckByID(id string, model interface{}) error {
 	int_id, err := utils.ConvertStringToUint(id)
@@ -13,7 +24,8 @@ func CheckByID(id string, model interface{}) error {
 
 	err = initializers.DB.First(model, int_id).Error
 	if err != nil {
-		return err
+		modelName := getModelName(model)
+		return fmt.Errorf("%s with ID: %d not found", modelName, int_id)
 	}
 
 	return nil
--- utils/stringConverter.go
+++ utils/stringConverter.go
@@ -1,14 +1,14 @@
 package utils
 
 import (
+	"fmt"
 	"strconv"
 )
 
 func ConvertStringToUint(s string) (uint, error) {
 	result, err := strconv.Atoi(s)
 	if err != nil {
-		// fmt.Println(err.Error())
-		return 0, err
+		return 0, fmt.Errorf("please enter valid ID")
 	}
 	return uint(result), nil
 }
